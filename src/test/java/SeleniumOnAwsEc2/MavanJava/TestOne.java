6d87e6d4c782c9961540055866eacab76f024110
asadbilu <41275778+asadbilu@users.noreply.github.com>
2021-04-05 01:11:25 -0400

    Add files via upload

--- src/test/java/SeleniumOnAwsEc2/MavanJava/TestOne.java
+++ src/test/java/SeleniumOnAwsEc2/MavanJava/TestOne.java
@@ -1,1 +1,29 @@
+package SeleniumOnAwsEc2.MavanJava;
 
+//package com.qascripts;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class TestOne {
+//	public static void main(String[] args)
+	@Test
+	public void OpenBrowser()
+	{	//"C:\\Users\\mursh\\Documents\\Udemy_Selenium\\Selenium\\chromedriver_89\\chromedriver.exe"
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+		
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("headless");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+	
+	
+	}
+
+}
